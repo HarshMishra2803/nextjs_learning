b31496b57c3ce82c31270fe6f4ce89dad5ddc6d1
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-10-03 21:06:06 +0530

    server_components

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-       i am a about page
+       <h1 className="text-xl">
+        problem solved by nextjs....
+       </h1>
+       <ul >
+        <li >Full stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional Feature like router from next/navigation</li>
+        <li>optimized Rendering </li>
+       </ul>
     </div>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
+"use client"
 import Image from "next/image";
 import React from "react";
+import {useState,useEffect} from "react"
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+
   return (
+
     <div>
-      i am a home page
+      i am a home page{count}
+      <button onClick={()=>setCount(count+1)}>click me</button>
     </div>
   );
 }
